b031d2028811f512f457d8af8ad69e641795c6fe
Alexandra McSween <alexmcsw@gmail.com>
2024-04-06 18:05:23 -0400

    plots!

--- astrological_analysis.R
+++ astrological_analysis.R
@@ -1,17 +1,19 @@
-devtools::install_github(repo = "sportsdataverse/fastRhockey")
+# devtools::install_github(repo = "sportsdataverse/fastRhockey")
+
 library(fastRhockey)
 library(tidyverse)
 library(DescTools)
 source("./fixed_functions.R")
 
 colours <- list(
-"BOS" = c("#154734", "#A2AAAD"),
-"MIN" = c("#2E1A47"),
-"MON" = c("#862633", "#333F48", "#DDCBA4"),
-"NY" = c("#A2AAAD", "#00B2A9", "#0C2340"),
-"OTT" = c("#333F48", "#A6192E"),
-"TOR" = c("#307FE2")
+"BOS" = "#154734",
+"MIN" = "#2E1A47",
+"MON" = "#862633",
+"NY" = "#00B2A9",
+"OTT" = "#A6192E",
+"TOR" = "#307FE2"
 )
+
 
 
 
@@ -51,3 +53,102 @@ team_stats <- function() {
 
 
 all_teams <- team_stats()
+
+# let's double check we didn't grab anyone twice:
+
+all_teams %>% get_dupes(contains("player_name"))
+
+# let's look at where our signs are distributed
+
+all_teams %>% group_by(sign, position.x) %>%
+  summarize(n = n())
+
+# let's look at the signs of our top scorers and top penalty-havers
+
+# Visualizations
+
+tail(all_teams[order(as.numeric(all_teams$goals)),], 10) %>%
+  ggplot(aes(sign, fill = team)) +
+  geom_bar() +
+  scale_fill_manual("legend", values = colours) +
+  ggtitle("Sign of top 10 scorers") +
+  theme(
+    plot.title = element_text(size=24)
+  )
+  
+tail(all_teams[order(as.numeric(all_teams$penalty_minutes)),], 10) %>%
+  ggplot(aes(sign, fill = team)) +
+  geom_bar() + 
+  scale_fill_manual(values = colours) +
+  ggtitle("Sign of top 10 penalty-havers") +
+  theme(
+    plot.title = element_text(size=24)
+  )
+
+# let's look at totals by sign
+
+all_teams %>% ggplot(aes(sign, fill = team)) +
+  geom_bar() + 
+  scale_fill_manual(values = colours) +
+  ggtitle("Signs of the PWHL by team") +
+  theme(
+    plot.title = element_text(size=24)
+  )
+
+all_teams %>% ggplot(aes(sign, goals, fill = team)) +
+  geom_bar(stat = "identity") + 
+  scale_fill_manual(values = colours) +
+  ggtitle("Signs of the PWHL by team") +
+  theme(
+    plot.title = element_text(size=24)
+  )
+
+
+
+all_teams %>% group_by(team, sign) %>%
+  summarize(goals = sum(as.numeric(goals))) %>%
+  ggplot(aes(sign, goals, fill = team)) +
+    geom_bar(stat = "identity") + 
+    scale_fill_manual(values = colours) +
+    ggtitle("Goals by sign") +
+    theme(
+      plot.title = element_text(size=24)
+    )
+
+# this could be skewed if there are many libras in the PWHL, for example.
+
+all_teams %>% group_by(team, sign) %>%
+  summarize(goals = sum(as.numeric(goals)), n = n()) %>%
+  mutate(adjusted_goals = goals / n) %>%
+  ggplot(aes(sign, adjusted_goals, fill = team)) +
+    geom_bar(stat = "identity") + 
+    scale_fill_manual(values = colours) +
+    ggtitle("Goals by sign") +
+    theme(
+      plot.title = element_text(size=24)
+    )
+
+
+# sim for penalty min
+
+all_teams %>% group_by(team, sign) %>%
+  summarize(penalty_minutes = sum(as.numeric(penalty_minutes))) %>%
+  ggplot(aes(sign, penalty_minutes, fill = team)) +
+    geom_bar(stat = "identity") + 
+    scale_fill_manual(values = colours) +
+    ggtitle("Penalty minutes by sign") +
+    theme(
+      plot.title = element_text(size=24)
+    )
+
+
+all_teams %>% group_by(team, sign) %>%
+  summarize(penalty_minutes = sum(as.numeric(penalty_minutes)), n = n()) %>%
+  mutate(adjusted_penalty_minutes = penalty_minutes / n) %>%
+  ggplot(aes(sign, adjusted_penalty_minutes, fill = team)) +
+    geom_bar(stat = "identity") + 
+    scale_fill_manual(values = colours) +
+    ggtitle("Penalty min by sign") +
+    theme(
+      plot.title = element_text(size=24)
+    )
